397b0fbacfc7ca15968ed6ef7fd48c56e9e7acf7
Ultra Zacky <ultrazacky@gmail.com>
2022-08-11 17:14:16 -0700

    add starting template

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Let's Play a Guessing Game!</h1>
+      <p>How good is your memory?</p>
+      <button>Start a New Game?</button>
+    </div>
+  )
+}
